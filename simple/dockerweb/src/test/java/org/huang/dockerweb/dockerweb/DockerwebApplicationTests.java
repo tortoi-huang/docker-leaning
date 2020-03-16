74333373eaef1703a275b30866c5d505dea60bdb
guancheng.huang <i-40@163.com>
2020-03-16 13:56:16 +0800

    初始化工程

---
+++ simple/dockerweb/src/test/java/org/huang/dockerweb/dockerweb/DockerwebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.huang.dockerweb.dockerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
