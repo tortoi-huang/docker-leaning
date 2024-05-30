7fe3c13e3549ff70e1900d67a6fc780e8245dcc1
tortoi <i-40@163.com>
2024-05-30 18:41:21 +0800

    feat office custome

--- compose/doris/office-custom/conf/init/init.sh
+++ compose/doris/office-custom/conf/init/init.sh
@@ -4,7 +4,6 @@
 # --skip-column-names --batch \
 # -e 'ALTER SYSTEM ADD FOLLOWER "fe02:9010";ALTER SYSTEM ADD FOLLOWER "fe03:9010";'
 
-
 # mysql --connect-timeout 10 -h fe01 -P 9030 -uroot \
 # --skip-column-names --batch \
 # -e 'ALTER SYSTEM ADD BACKEND "be01:9050";ALTER SYSTEM ADD BACKEND "be02:9050";ALTER SYSTEM ADD BACKEND "be03:9050";'
